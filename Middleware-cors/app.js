ca4498b758160763df544cd4ff24aa4682bddb82
Ramdeniakki <ramdeniakki35@gmail.com>
2024-10-16 19:34:21 +0530

    added Middleware and Cors

--- Middleware-cors/app.js
+++ Middleware-cors/app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express();
 
-app.get('/sum',function(req,res){
-    const a = parseInt(req.query.a)
-    const b = parseInt(req.query.b);
+// let requestcount = 0;
+
+// const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+app.post('/sum',function(req,res){
+
+    // requestcount = requestcount+1;
+    // console.log(`request count  + ${requestcount}`);
+
+    const a = parseInt(req.body.a)
+    const b = parseInt(req.body.b);
 res.json({
     ans: a+b
 })
