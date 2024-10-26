6b8da5196493231451e3efe8c6a6f87c7c65f3cf
Ramdeniakki <ramdeniakki35@gmail.com>
2024-10-26 20:53:13 +0530

    added mongoose

---
+++ Mongodb/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { emit } = require('nodemon');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+const users = new Schema({
+    email:String,
+    password:String,
+    name:String
+})
+
+const todo  = new Schema({
+    title:String,
+    done:Boolean
+})
+
+const UserModel = mongoose.model("user",users);
+const Todomodel = mongoose.model("todo",todo);
+
+module.exports = {
+    UserModel:UserModel,
+    Todomodel:Todomodel
+}
---
+++ Mongodb/index.js
@@ -0,0 +1,55 @@
+const {UserModel,Todomodel} = require('./db');
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require("jsonwebtoken")
+const JWT_SECERET = "testingemail.come";
+mongoose.connect("mongodb+srv://admin:admin%400701@cluster0.xujxl.mongodb.net/todo-app-database");
+const app = express();
+app.use(express.json());
+app.post('/signup',async function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+    await UserModel.create({
+        email:email,
+        password:password,
+        name:name
+    })
+    res.json({
+        msg:"You are signed up"
+    })
+})
+
+app.post("/signin",async function(req,res){
+    const email= req.body.email
+    const password = req.body.password
+    const user = await UserModel.findOne({
+        email:email,
+        password:password
+    })
+    console.log(user);
+
+    if(user){
+        const token = jwt.sign({
+            id:user._id
+        },JWT_SECERET);
+        res.json({
+            token:token
+        })
+    }else{
+        res.status(403).json({
+            msg:"incorrect Credentials"
+        })
+    }
+})
+
+app.post('/todo',function(req,res){
+
+})
+
+app.get('/todos',function(req,res){
+
+})
+
+
+app.listen(3000);
